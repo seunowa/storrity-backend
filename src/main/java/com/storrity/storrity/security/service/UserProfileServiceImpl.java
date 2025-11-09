f2696d74c315a8f08b2240371a7b65bc1353581c
seunowa <seunowa@yahoo.com>
2025-11-09 15:52:15 +0100

    Fix transaction error in edit user profile info

--- src/main/java/com/storrity/storrity/security/service/UserProfileServiceImpl.java
+++ src/main/java/com/storrity/storrity/security/service/UserProfileServiceImpl.java
@@ -16,6 +16,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
@@ -53,6 +54,7 @@ public class UserProfileServiceImpl implements UserProfileService{
         return new CountDto(count);
     }
 
+    @Transactional
     @Override
     public UserProfileDto update(String username, UserProfileUpdateDto dto) {
         UserProfile userProfile = userProfileRepository.findByUsernameForUpdate(username)
